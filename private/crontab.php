e866589270e61c2509e13c95f913c20a65fdae65
citizenz <citizenz7@protonmail.com>
2019-08-20 13:45:30 +0200

    Update crontab.php

--- private/crontab.php
+++ private/crontab.php
@@ -1,5 +1,13 @@
 <?php
 
+// Ce fichier est destiné à tourner en tâche cron afin de "vider" la table xbt_announce_log toutes les heures (59 min !)
+// crontab.php sert au fichier peers.php afin d'afficher les clients connectés, leurs configurations, etc.
+// J'ai choisi de mettre ce fichier en dehors du path /web du site dans /private
+// Mes différents rep sont donc :
+// /var/www/ft4a.xyz/web, /var/www/ft4a.xyz/private, /var/www/ft4a.xyz/logs
+//J'ai jouté sous root (en faisant crontab -e) :
+// 0 */2 * * * php /var/www/ft4a.xyz/private/crontab.php
+
 //Identifiants SQL
 define('DBHOST','localhost');
 define('DBUSER','xxxxxxxxx');
