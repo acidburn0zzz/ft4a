74a3ad341be33c4a375b34c577207e4cb584f4eb
citizenz <citizenz7@protonmail.com>
2019-08-28 06:42:47 +0200

    Auto logout session fix (3)

--- logout.php
+++ logout.php
@@ -2,16 +2,23 @@
 require_once 'includes/config.php';
 
 if (isset($_SESSION['username'])) {
-	//log user out
-	$user->logout();
-	//on supprime le user de la base SQL connectes
-	$stmt = $db->prepare('DELETE FROM connectes WHERE pseudo = :pseudo') ;
-	$stmt->execute(array(
-    		':pseudo' => $_SESSION['username']
-	));
+        //log user out
+        $user->logout();
+        //on supprime le user de la base SQL connectes
+        $stmt = $db->prepare('DELETE FROM connectes WHERE pseudo = :pseudo') ;
+        $stmt->execute(array(
+                ':pseudo' => $_SESSION['username']
+        ));
+
+        //Message de déconnexion...
+        if(isset($_GET['action']) && $_GET['action'] == 'deco'){
+                header('Location: login.php?action=deco');
+                exit;
+        }
+        else {
+                //on renvoie sur la page login
+                header('Location: login.php');
+                exit;
+        }
 }
-
-//on renvoie sur la page login
-header('Location: login.php'); 
-
 ?>
